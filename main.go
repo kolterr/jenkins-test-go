03b019597171e1d176009daef1e7ff3debd0b6f8
汪洋 <lanqiu77632>
2018-06-01 11:13:33 +0800

    test

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
-import "net/http"
+import (
+	"github.com/labstack/echo"
+)
 
 func main() {
+	e := echo.New()
+	e.GET("/", func(context echo.Context) error {
+		return context.JSON(200, "Hello world!")
+	})
+	e.Start(":7777")
 
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/", hello)
-	http.ListenAndServe(":7777", mux)
-}
-
-func hello(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello jenkins!"))
 }
